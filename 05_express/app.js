4b189e7c182635c129ba083916992fb039ebe47b
SOROKKIM <ksl7593@gmail.com>
2024-01-24 17:00:52 +0900

    동기와 비동기 외 수정한 코드들 - 04_node파일 에러존재

--- 05_express/app.js
+++ 05_express/app.js
@@ -11,8 +11,32 @@ app.use("/static", express.static(__dirname + "/public"));
 
 app.get("/", (requset, response) => {
   //   response.send("hello express!! 안녕하세요 익스프레스");
-  response.render("index.ejs");
+
+  // render의 두번째 인자에서 index.ejs에서 사용할 정보 전달
+  response.render("index.ejs", {
+    btns: ["apple", "banana"],
+    isLogin: false,
+    userInfo: {
+      name: "boa",
+      msg: "점심 먹었나요?",
+    },
+  });
 });
+
+//라우팅
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//page not found 404 페이지는 맨 마지막에 설정
+app.get("*", (req, res) => {
+  res.render("404");
+});
+
 app.listen(PORT, function () {
   console.log("http://localhost:" + PORT);
 });

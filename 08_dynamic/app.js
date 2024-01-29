1d101cb5e4638c0753fb051fbbf257ef58f10ca9
SOROKKIM <ksl7593@gmail.com>
2024-01-29 16:28:23 +0900

    axios Get, Post 실습문제 1,2

--- 08_dynamic/app.js
+++ 08_dynamic/app.js
@@ -4,6 +4,7 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+// app.set("views", "./views/practice");
 //미들웨어 - body로 데이터를 주고 받을 수 있음
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json()); //json 형식으로 데이터를 주고 받음
@@ -53,6 +54,31 @@ app.post("/fetch", (req, res) => {
 //open api 사용
 app.get("/open-api", (req, res) => {
   res.render("api");
+});
+
+//실습1
+app.get("/practice1", (req, res) => {
+  res.render("practice/practice1.ejs");
+});
+
+//실습2
+app.get("/practice2", (req, res) => {
+  res.render("practice/practice2.ejs");
+});
+
+// 간단한 ID와 PW 변수로 지정
+const validId = "dd";
+const validPw = "123";
+
+app.post("/practice2", (req, res) => {
+  const { id, pw } = req.body;
+
+  // 간단한 로그인 처리
+  if (id === validId && pw === validPw) {
+    res.status(200).json({ success: true, message: "로그인 성공 야호" });
+  } else {
+    res.status(200).json({ success: false, message: "로그인 실패 ㅠㅠ" });
+  }
 });
 
 app.listen(PORT, () => {

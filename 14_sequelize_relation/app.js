4f4707c6ed3f62c3b74591191b12b966c6ffcbf7
SOROKKIM <ksl7593@gmail.com>
2024-02-19 23:11:10 +0900

    sequelize 실습 및 relation(관계매핑)

---
+++ 14_sequelize_relation/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { sequelize } = require("./models");
+const app = express();
+const PORT = 8080;
+const router = require("./routes");
+
+//middleware
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/", router);
+
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ 14_sequelize_relation/models/TeamGame.js
@@ -0,0 +1,17 @@
+const TeamGameModel = (sequelize, DataTypes) => {
+  const TeamGame = sequelize.define(
+    "TeamGame",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+    },
+    {
+      freezeTableName: true,
+    }
+  );
+  return TeamGame;
+};
+module.exports = TeamGameModel;

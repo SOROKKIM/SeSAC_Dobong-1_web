32017edcf677b2ee5308436165eef75fedc5c771
SOROKKIM <ksl7593@gmail.com>
2024-02-23 15:38:10 +0900

    공인 아이피 주소 수정

--- 16_jwt/app.js
+++ 16_jwt/app.js
@@ -79,5 +79,5 @@ app.post("/token", (req, res) => {
 });
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
-  console.log(`http://http://101.101.210.8:${PORT}`);
+  console.log(`http://101.101.210.8:${PORT}`);
 });

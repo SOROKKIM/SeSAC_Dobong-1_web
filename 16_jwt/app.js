0d38500c32ba38f6190d5978e07e80bf7c06f23d
SOROKKIM <ksl7593@gmail.com>
2024-02-23 15:16:02 +0900

    bearer 토큰 사용

--- 16_jwt/app.js
+++ 16_jwt/app.js
@@ -45,7 +45,38 @@ app.post("/login", (req, res) => {
 });
 
 //token 정보 확인
-app.post("/token", (req, res) => {});
+app.post("/token", (req, res) => {
+  try {
+    if (req.headers.authorization) {
+      console.log(req.headers.authorization);
+      //Bearer 빼고 token 값만 저장하기 위해서
+      const token = req.headers.authorization.split(" ")[1];
+
+      try {
+        console.log("token:::", token); //token::: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNvY29hIiwiaWF0IjoxNzA4NjU2MDI0fQ.TjLDn_y6iTxRS2Q_T5MYg7Rmykd_By0Z90OGE98StRE
+        const auth = jwt.verify(token, SECRET);
+        console.log("auth:::", auth);
+        // verify()의 리턴값 => auth::: { id: 'cocoa', iat: 1708656024 }
+        // iat : issued at, 발급된 시간, 토큰이 만든지 얼만큼 됐는지 알수있음
+
+        if (userInfo.id === auth.id) {
+          res.send({ result: true, name: userInfo.name });
+        }
+        res.end();
+      } catch (err) {
+        // 잘못된 정보가 들어왔을 때
+        console.log("토큰 인증 에러", err);
+        res.send({ result: false, message: "인증된 회원이 아닙니다." });
+      }
+    } else {
+      // 인증 정보 안들어왔을때
+      res.redirect("/login");
+    }
+  } catch (err) {
+    console.log("POST /token", err);
+    res.status(500).send("server error");
+  }
+});
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
